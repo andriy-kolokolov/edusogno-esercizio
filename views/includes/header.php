091000a3df2cdcc9bf55b788db347164b09cea8e
andriy-kolokolov <koloandre22@gmail.com>
2023-10-03 20:31:44 +0200

    add styles, responsive header

--- views/includes/header.php
+++ views/includes/header.php
@@ -3,22 +3,34 @@
 use Util\Auth;
 
 ?>
-<header>
-    <div class="container">
-        <h1>Edusogno</h1>
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
+<header class="header">
+    <div class="header__container">
+        <div class="header__logo-wrapper">
+            <img class="logo-wrapper__img" src="../../assets/img/logo.png" alt="logo image">
+        </div>
+        <nav class="header__nav">
+            <ul class="nav__list">
+                <li class="list__item">
+                    <a class="item__link" href="/">Home</a>
+                </li>
+                <!-- Authenticated user navigation -->
                 <?php if (Auth::user()) { ?>
-                    <li><a href="/dashboard">Dashboard</a></li>
-                    <li>
-                        <form action="auth/logout" method="post">
+                    <li class="list__item">
+                        <a class="item__link" href="/dashboard">Dashboard</a>
+                    </li>
+                    <li class="list__item">
+                        <form class="item__logout-form" action="auth/logout" method="post">
                             <input type="submit" name="logout" value="Logout">
                         </form>
                     </li>
                 <?php } else { ?>
-                    <li><a href="/login">Log In</a></li>
-                    <li><a href="/register">Register</a></li>
+                    <!-- Not authenticated user navigation -->
+                    <li class="list__item">
+                        <a class="item__link" href="/login">Log In</a>
+                    </li>
+                    <li class="list__item">
+                        <a class="item__link" href="/register">Register</a>
+                    </li>
                 <?php } ?>
             </ul>
         </nav>
